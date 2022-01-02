43c80d9e2c492aab52552c1c2521da958fa81f3d
Cedrick F. Campoto <cedrickcampoto@yahoo.com>
2022-01-02 22:22:39 +0800

    ""

--- dist/cake.js
+++ dist/cake.js
@@ -2848,13 +2848,14 @@
               let id2 = `ckf${this.uiid}`;
               let el3 = els[f];
               let fr2 = el3.dataset.for;
-              let isCleaned = el3.dataset.cleaned;
+              let isCleaned = el3.dataset.cleaned === true;
               let [a, b, c] = fr2.split(" ");
               el3.style.display = "none";
               el3.classList.add("cake-template");
               el3.dataset.for = id2;
               el3.dataset.forTemplate = id2;
-              o2[id2] = { bind: c, sel: id2, iter: a, ins: b, cleaned: isCleaned == "true" };
+              console.log(753, isCleaned, el3.dataset.cleaned);
+              o2[id2] = { bind: c, sel: id2, iter: a, ins: b, cleaned: isCleaned };
               ++this.uiid;
               if (f != 0) {
                 let parent = el3.parentElement && el3.parentElement.closest("[data-for]");
