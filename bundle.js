b1d23aa813da616d6f0e617a42cdbfac2f2c8735
spadefaith <cedrickcampoto@yahoo.com>
2022-08-21 22:12:59 +0800

    update

--- bundle.js
+++ bundle.js
@@ -2,7 +2,7 @@ require('esbuild').build({
     entryPoints: ['src/app.js'],
     bundle: true,
     outfile: 'dist/cfc.js',
-    // minify:true,
+    minify:true,
     target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
   }).catch(() => process.exit(1))
 
